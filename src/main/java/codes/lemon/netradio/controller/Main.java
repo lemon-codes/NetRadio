0e23577c66a6ca56a0eb02035d2283531107df7c
lemon-codes <dev@lemon.codes>
2021-02-25 05:54:31 +0000

    Previously the model was shutdown on application exit by registering an action listener with the primary stage. We know take the more conventional approach of overriding JavaFX Applications stop() method with an implementation that calls the models shutdown() method. Also moved the Main class to controller package.

--- src/main/java/codes/lemon/netradio/controller/Main.java
+++ src/main/java/codes/lemon/netradio/controller/Main.java
@@ -1,4 +1,4 @@
-package codes.lemon.netradio.view;
+package codes.lemon.netradio.controller;
 
 import codes.lemon.netradio.controller.ModelAdapterImpl;
 import javafx.application.Application;
@@ -7,24 +7,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("root.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/root.fxml"));
         primaryStage.setTitle("NetRadio");
         primaryStage.setScene(new Scene(root));
-
-        // quickfix to ensure the model is shut down properly
-        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent windowEvent) {
-                ModelAdapterImpl.getInstance().shutdown();
-            }
-        });
         primaryStage.show();
+    }
+
+    @Override
+    public void stop() {
+        // shut down model on Application exit.
+        ModelAdapterImpl.getInstance().shutdown();
+
     }
 
 
--- src/main/java/codes/lemon/netradio/controller/ModelEventHandler.java
+++ src/main/java/codes/lemon/netradio/controller/ModelEventHandler.java
@@ -1,5 +1,9 @@
 package codes.lemon.netradio.controller;
 
+/**
+ * Classes which implement this interface can subscribe to the ModelAdapter
+ * to be notified when other components in the system alter the models state.
+ */
 interface ModelEventHandler {
     void handleEvent(ModelAdapter.ModelEvent event);
 }
