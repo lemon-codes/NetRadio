b2470ec2819169cb413e11e1f02e99e6f1ec8424
lemon-codes <dev@lemon.codes>
2021-04-18 19:30:14 +0100

    Basic recording pipelines can now be constructed in PlayBinFactory

--- src/main/java/codes/lemon/netradio/model/PlayBinFactory.java
+++ src/main/java/codes/lemon/netradio/model/PlayBinFactory.java
@@ -1,6 +1,6 @@
 package codes.lemon.netradio.model;
 
-import org.freedesktop.gstreamer.Gst;
+import org.freedesktop.gstreamer.*;
 import org.freedesktop.gstreamer.elements.PlayBin;
 
 import java.io.File;
@@ -45,7 +45,7 @@ class PlayBinFactory {
      * @param source audio source
      * @param fileName name of output file
      * @param fileType audio format of output file
-     * @return
+     * @return a PlayBin which stores the audio on disk.
      */
     public static PlayBin buildRecordingPlayBin(URI source, File fileName, AUDIO_FORMAT fileType) {
         // always check if Gstreamer is initialised since other components could uninitialise
@@ -53,8 +53,54 @@ class PlayBinFactory {
             Gst.init();
             System.out.println("Gst initialised");
         }
-        // TODO: implement recording
-        return new PlayBin("Recording");
+
+        PlayBin pb = new PlayBin("Recording");
+        pb.setURI(source);
+        switch (fileType) {
+            case MP3 -> pb.setAudioSink(buildMP3DiskSink());
+        }
+        return pb;
+    }
+
+
+    /**
+     * Builds a pipeline which can be used as an audio sink.
+     * This implementation records playback to disk as an mp3 file.
+     * // TODO: works perfectly
+     * @return an audio sink which writes data to disk in mp3 format
+     */
+    private static Bin buildMP3DiskSink() {
+        /*
+           source -> playbin -> queue -> audioconvert -> lamemp3enc -> id3v2mux -> fileSink
+         */
+        Bin multipleAudioSinkBin = new Bin();
+
+        Element diskQueue = ElementFactory.make("queue", "diskQueue");
+        Element audioConverter = ElementFactory.make("audioconvert", "audioConverter");
+        Element mp3Encoder = ElementFactory.make("lamemp3enc", "mp3Encoder");
+        // Adds an ID3v2 header to the beginning of MP3 files using taglib
+        Element mp3MetadataFormatter = ElementFactory.make("id3v2mux", "mp3MetadataFormatter");
+        Element diskSink = ElementFactory.make("filesink", "diskSink");
+
+        // TODO: use client supplied out file
+        diskSink.set("location", "./output.mp3");
+
+        multipleAudioSinkBin.add(diskQueue);
+        multipleAudioSinkBin.add(audioConverter);
+        multipleAudioSinkBin.add(mp3Encoder);
+        multipleAudioSinkBin.add(mp3MetadataFormatter);
+        multipleAudioSinkBin.add(diskSink);
+
+        GhostPad gPad = new GhostPad("sink", PadDirection.SINK);
+        gPad.setTarget(diskQueue.getSinkPads().get(0));
+        multipleAudioSinkBin.addPad(gPad);
+
+        // diskQueue -> audioconvert -> mp3encode -> mp3formatter -> diskSink
+        diskQueue.link(audioConverter);
+        audioConverter.link(mp3Encoder);
+        mp3Encoder.link(mp3MetadataFormatter);
+        mp3MetadataFormatter.link(diskSink);
+        return multipleAudioSinkBin;
     }
 
     /**
@@ -67,7 +113,7 @@ class PlayBinFactory {
      * @param source audio source
      * @param fileName name of output file
      * @param fileType format of output file
-     * @return
+     * @return a PlayBin which simultaneously plays audio while storing the audio on disk
      */
     public static PlayBin buildRecordingPlaybackPlayBin(URI source, File fileName, AUDIO_FORMAT fileType) {
         // always check if Gstreamer is initialised since other components could uninitialise
@@ -76,7 +122,71 @@ class PlayBinFactory {
             System.out.println("Gst initialised");
         }
         // TODO: implement recording
-        return new PlayBin("Recording");
+        PlayBin pb = new PlayBin("Recording");
+        pb.setURI(source);
+        switch(fileType) {
+            case MP3 -> pb.setAudioSink(buildMP3AudioDiskSink());
+        }
+        return pb;
+    }
+
+
+    /**
+     * Builds a pipeline which can be used as an audio sink.
+     * This implementation plays audio through the soundcard whilst
+     * simultaneously recording playback to disk as an mp3 file.
+     * // TODO: works perfectly
+     * @return an audio sink which simultaneously passes data to the sound card for
+     *          live playback and stores a copy of the audio on disk in mp3 format
+     */
+    private static Bin buildMP3AudioDiskSink() {
+        /*
+                                    |-> queue -> autoaudiosink
+           source -> playbin -> tee-|
+                                    |-> queue -> audioconvert -> lamemp3enc -> id3v2mux -> fileSink
+
+         */
+        Bin multipleAudioSinkBin = new Bin();
+
+        Element tee = ElementFactory.make("tee", "audioDuplicator");
+        Element playbackQueue = ElementFactory.make("queue", "playbackQueue");
+        Element diskQueue = ElementFactory.make("queue", "diskQueue");
+        Element playbackSink = ElementFactory.make("autoaudiosink", "playbackSink");
+        Element audioConverter = ElementFactory.make("audioconvert", "audioConverter");
+        Element mp3Encoder = ElementFactory.make("lamemp3enc", "mp3Encoder");
+
+        // Adds an ID3v2 header to the beginning of MP3 files using taglib
+        Element mp3MetadataFormatter = ElementFactory.make("id3v2mux", "mp3MetadataFormatter");
+        Element diskSink = ElementFactory.make("filesink", "diskSink");
+        diskSink.set("location", "./output.mp3");
+
+        multipleAudioSinkBin.add(tee);
+        multipleAudioSinkBin.add(playbackQueue);
+        multipleAudioSinkBin.add(playbackSink);
+        multipleAudioSinkBin.add(diskQueue);
+        multipleAudioSinkBin.add(audioConverter);
+        multipleAudioSinkBin.add(mp3Encoder);
+        multipleAudioSinkBin.add(mp3MetadataFormatter);
+        multipleAudioSinkBin.add(diskSink);
+
+        GhostPad gPad = new GhostPad("sink", PadDirection.SINK);
+        gPad.setTarget(tee.getSinkPads().get(0));
+        multipleAudioSinkBin.addPad(gPad);
+
+        // duplicate the audio to form two pipelines
+        tee.link(playbackQueue);
+        tee.link(diskQueue);
+
+        // playbackQueue -> playbackSink
+        playbackQueue.link(playbackSink);
+
+        // diskQueue -> audioconvert -> mp3encode -> mp3formatter -> diskSink
+        diskQueue.link(audioConverter);
+        audioConverter.link(mp3Encoder);
+        mp3Encoder.link(mp3MetadataFormatter);
+        mp3MetadataFormatter.link(diskSink);
+        multipleAudioSinkBin.debugToDotFile(Bin.DebugGraphDetails.SHOW_ALL, "/tmp/debug-pipeline.DOT");
+        return multipleAudioSinkBin;
     }
 
 }
